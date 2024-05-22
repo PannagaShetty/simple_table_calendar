4a0e089fd275a719eaf8d58a3a4961f0c9b73ba7
pannagaShettyVistas <pannaga@vistasglobal.com>
2024-05-22 13:02:55 +0530

    added example

---
+++ example/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:simple_table_calendar/simple_table_calendar.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+            child: SizedBox(
+          height: 350,
+          child: TableCalender(
+            firstDay: DateTime(2021, 1, 1),
+            lastDay: DateTime(2021, 12, 31),
+            selectedDay: DateTime(2021, 1, 1),
+            onDaySelected: (DateTime day) => print(day),
+          ),
+        )),
+      ),
+    );
+  }
+}
